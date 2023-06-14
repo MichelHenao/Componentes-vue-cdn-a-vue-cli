781b931aec8ef714c62f8bd7777558ef8bc9ff0f
MichelHenao <michelvaleria0510@gmail.com>
2023-06-13 19:56:46 -0500

    desafio componentes con vue cli

---
+++ project-name/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <MiCatalogo :peliculas="peliculas"></MiCatalogo>
+  </div>
+</template>
+
+<script>
+import MiCatalogo from './components/MiCatalogo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MiCatalogo
+  },
+  data() {
+    return {
+      peliculas: [
+        {
+          id: 1,
+          imagen: './assets/titanik.jpg',
+          titulo: 'Titanic',
+          duracion: '3h 14m',
+          genero: 'Romance, Drama',
+          director: 'James Cameron'
+        },
+        {
+          id: 2,
+          imagen: './assets/theshaws.jpg',
+          titulo: 'The Shawshank Redemption',
+          duracion: '2h 22m',
+          genero: 'Drama, Crime',
+          director: 'Frank Darabont'
+        },
+        {
+          id: 3,
+          imagen: './assets/godfather.jpg',
+          titulo: 'The Godfather',
+          duracion: '2h 55m',
+          genero: 'Crime, Drama',
+          director: 'Francis Ford Coppola'
+        },
+        {
+          id: 4,
+          imagen: './assets/pulfiction.jpg',
+          titulo: 'Pulp Fiction',
+          duracion: '2h 34m',
+          genero: 'Crime, Drama',
+          director: 'Quentin Tarantino'
+        },
+      ]
+    };
+  }
+}
+</script>
+
---
+++ project-name/src/components/MiCatalogo.vue
@@ -0,0 +1,63 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Imagen</th>
+        <th>Título</th>
+        <th>Duración</th>
+        <th>Género</th>
+        <th>Director</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="pelicula in peliculas" :key="pelicula.id">
+        <td><img :src="pelicula.imagen" :alt="pelicula.titulo" width="150"></td>
+        <td>{{ pelicula.titulo }}</td>
+        <td>{{ pelicula.duracion }}</td>
+        <td>{{ pelicula.genero }}</td>
+        <td>{{ pelicula.director }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'MiCatalogo',
+  props: {
+    peliculas: {
+      type: Array,
+      required: true
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+.table {
+    width: 100%;
+    height: 10px;
+    margin-bottom: 1rem;
+    font-size: 20px;
+    text-align: center;
+  }
+  
+  .table th,
+  .table td {
+    padding: 0.5rem;
+    background-color: black;
+    color: white;
+  }
+  
+  .table th {
+    background-color: #9d0208;
+    font-weight: bold;
+    text-align: center;
+  }
+  
+  .table td img {
+    max-width: 150px;
+    height: auto;
+  }
+</style>
